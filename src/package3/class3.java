c6cd0fd7ee649765c446f5f3efd184ba39d1b7a2
Knolchie <haasmatze@gmail.com>
2018-11-08 15:26:11 +0100

    aufgabe 3 uund 4

---
+++ src/package3/class3.java
@@ -0,0 +1,19 @@
+package package3;
+
+public class class3
+{
+    public static void main(String[] args)
+    {
+        String x= "matteo";
+        int y = 18;
+        System.out.println(x);
+        System.out.println(y);
+
+    /*variablenname wurde auf fhKufsteinTirol gekürzt*/
+
+    String fachhochschuleKufsteinTirol= "fhKufsteinTirol";
+
+    final String FH_KUFSTEIN_TIROL= "fachhochschuleKufsteinTirol";
+
+
+}
